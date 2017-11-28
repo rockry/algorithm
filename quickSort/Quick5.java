e086c284c69c3e6766b41b8ac00c6a3d5129770e
sanghyuk.nam <sanghyuk.nam@lge.com>
2017-11-28 11:08:26 +0900

    20171128 quicksort5 print add

--- quickSort/Quick5.java
+++ quickSort/Quick5.java
@@ -8,11 +8,13 @@ public class Quick5 {
                 while (arr[i] < pivot) i++;
                 while (arr[j] > pivot) j--;
                 if (i <= j) {
+                      printArray(arr, i, j, left, right, pivot);
                       tmp = arr[i];
                       arr[i] = arr[j];
                       arr[j] = tmp;
                       i++;
                       j--;
+                      printArray(arr, i, j, left, right, pivot);
                 }
           };
          
@@ -31,12 +33,29 @@ public class Quick5 {
         // int data[] = {66, 10, 1, 34, 5, -10, 8, 9};
 		// int data[] = {9,3,8,5,2,1,6,4};
         // int data[] = {3,4,9};
-        int data[] = {9,4,3};
+      //   int data[] = {9,4,3};
+      int data[] = {2,9,3,8,7,6,5,13,1,11};
         
         Quick5 quick = new Quick5();
         quick.quickSort(data, 0, data.length - 1);
         for(int i=0; i<data.length; i++){
             System.out.println("data["+i+"] : "+data[i]);
         }
+    }
+
+    	public void printArray(int[] arr, int i, int j, int left, int right, int pivot) {
+		for(int n = 0; n < arr.length; n++) {
+			if(n == left) System.out.print("(");
+            if(n == i && n == j) {
+                System.out.print("*");
+            } else if(n == i) {
+                System.out.print("i");
+            } else if(n == j) {
+                System.out.print("j");
+            }
+            System.out.print(arr[n] + " ");
+			if(n == right) System.out.print(")");
+		}
+		System.out.println(" pivot = " + pivot);
     }
 }
